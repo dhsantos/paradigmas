b859b721df499b96413d53570ac1e9bb0b86b9ee
Diego Santos <diego.h.santos@gmail.com>
2025-09-13 04:02:51 -0300

    Add files via upload

---
+++ tp1-base/src/Util.hs
@@ -0,0 +1,26 @@
+module Util where
+
+-- | @alinearDerecha n s@ agrega espacios a la izquierda de @s@ hasta que su longitud sea @n@.
+-- Si @s@ ya tiene longitud @>= n@, devuelve @s@.
+alinearDerecha :: Int -> String -> String
+alinearDerecha n s
+                | length s >= n = s
+                | otherwise = take (n - length s) (repeat ' ') ++ s
+
+-- | Dado un índice y una función, actualiza el elemento en la posición del índice
+-- aplicando la función al valor actual. Si el índice está fuera de los límites
+-- de la lista, devuelve la lista sin cambios.
+-- El primer elemento de la lista es el índice 0.
+actualizarElem :: Int -> (a -> a) -> [a] -> [a]
+actualizarElem n f xs 
+                | length xs < n  = xs 
+                | otherwise = zipWith (\i x -> if i == n then f x else x) [0..] xs
+
+
+-- | infinito positivo (Haskell no tiene literal para +infinito)
+infinitoPositivo :: Float
+infinitoPositivo = 1 / 0
+
+-- | infinito negativo (Haskell no tiene literal para -infinito)
+infinitoNegativo :: Float
+infinitoNegativo = -(1 / 0)
