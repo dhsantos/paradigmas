dc759d417edf7fb06feb1435347bf343fb57e8f5
dsantos <dsantos@telecentro.net.ar>
2025-11-08 17:35:15 -0300

    Se agrega tp2

---
+++ tp-prolog-base/nonograma.pl
@@ -0,0 +1,214 @@
+
+
+% Ejercicio 1
+matriz(F, C, M) :- 
+    length(M, F),
+    maplist(fila_de_longitud(C), M).       
+
+fila_de_longitud(C, Fila) :-
+    length(Fila, C).
+
+
+% Ejercicio 2
+replicar(X, N, L) :-
+    length(L, N),
+    maplist(=(X), L).
+
+% Ejercicio 3
+transponer(M, MT) :-
+	nth1(1, M, FirstRow),
+	length(FirstRow, ColCount),
+	replicar([], ColCount, MT0),
+	foldl(agregar_a_columna, M, MT0, MT).
+agregar_a_columna([], [], []).
+agregar_a_columna([HR | TR], [HM0 | TM0], [HMT | TMT]) :-
+	agregar_a_columna(TR, TM0, TMT),
+	append(HM0, [HR], HMT).
+
+armarNono(RF, RC, nono(M, RS)) :-
+	length(RF, F),
+	length(RC, C),
+	matriz(F, C, M),
+	transponer(M, Mt),
+	zipR(RF, M, RSFilas),
+	zipR(RC, Mt, RSColumnas),
+	append(RSFilas, RSColumnas, RS).
+
+zipR([], [], []).
+zipR([R|RT], [L|LT], [r(R,L)|T]) :- zipR(RT, LT, T).
+
+% Ejercicio 4
+pintadasValidas(r(Res, Celdas)) :-
+    pintar_con_restricciones(Res, Celdas),
+    verificar_longitud(Res, Celdas).
+pintar_con_restricciones([], Celdas) :-
+    todas_o(Celdas).
+
+pintar_con_restricciones([R|Rs], Celdas) :-
+    append(Prefix, Resto, Celdas),
+    todas_o(Prefix), 
+    append(Bloque, Sufijo, Resto),
+    replicar(x, R, Bloque), 
+    pintar_siguiente(Rs, Sufijo). 
+
+pintar_siguiente([], Celdas) :-
+    todas_o(Celdas).
+
+pintar_siguiente([R|Rs], Celdas) :-
+    Celdas = [o|Resto],
+    pintar_con_restricciones([R|Rs], Resto).
+
+todas_o([]).
+todas_o([o|Resto]) :-
+    todas_o(Resto).
+
+verificar_longitud(Res, Celdas) :-
+    sum_list(Res, Pintadas),
+    length(Res, K),
+    length(Celdas, N),
+    Minimo is Pintadas + K - 1,
+    N >= Minimo.
+
+% Ejercicio 5
+resolverNaive(nono(_, Restricciones)) :-
+    maplist(pintadasValidas, Restricciones).
+
+% Ejercicio 6
+pintarObligatorias(R) :-
+	R = r(Restric, Linea),
+	findall(Linea, pintadasValidas(r(Restric, Linea)), PosiblesLineas),
+	transponer(PosiblesLineas, Lineas),
+	lineasObligatorias(Lineas, Linea).
+
+% AUXILIAR - recibe una lista de lineas (cada línea representando valores de una columna) y
+% arma una lista resultado, donde cada celda se fija a un valor (si la línea tiene el mismo
+% valor en todas las celdas), sino deja la celda sin modificar.
+%! lineasObligatorias(+Lineas, ?Linea)
+lineasObligatorias([], []).
+lineasObligatorias([HLs | TLs], [HL | TL]) :-
+	lineasObligatorias(TLs, TL),
+	list_to_set(HLs, Valores),
+	reducirValorObligatorio(Valores, HL).
+
+% AUXILIAR - recibe una lista de valores únicos (que aparecían en las posibles pintadas). Si todos
+% son el mismo valor (la lista tiene longitud 1), entonces se fija a ese valor. Sino se deja la variable
+% sin modificar.
+%! reducirValorObligatorio(+Valores, +Variable)
+reducirValorObligatorio([], _).
+reducirValorObligatorio([V], HL) :- HL = V.
+reducirValorObligatorio([_, _ | _], _).
+
+
+% Predicado dado combinarCelda/3
+combinarCelda(A, B, _) :- var(A), var(B).
+combinarCelda(A, B, _) :- nonvar(A), var(B).
+combinarCelda(A, B, _) :- var(A), nonvar(B).
+combinarCelda(A, B, A) :- nonvar(A), nonvar(B), A = B.
+combinarCelda(A, B, _) :- nonvar(A), nonvar(B), A \== B.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Ejercicio 7
+deducir1Pasada(_) :- completar("Ejercicio 7").
+
+% Predicado dado
+cantidadVariablesLibres(T, N) :- term_variables(T, LV), length(LV, N).
+
+% Predicado dado
+deducirVariasPasadas(NN) :-
+	NN = nono(M,_),
+	cantidadVariablesLibres(M, VI), % VI = cantidad de celdas sin instanciar en M en este punto
+	deducir1Pasada(NN),
+	cantidadVariablesLibres(M, VF), % VF = cantidad de celdas sin instanciar en M en este punto
+	deducirVariasPasadasCont(NN, VI, VF).
+
+% Predicado dado
+deducirVariasPasadasCont(_, A, A). % Si VI = VF entonces no hubo más cambios y frenamos.
+deducirVariasPasadasCont(NN, A, B) :- A =\= B, deducirVariasPasadas(NN).
+
+% Ejercicio 8
+restriccionConMenosLibres(_, _) :- completar("Ejercicio 8").
+
+% Ejercicio 9
+resolverDeduciendo(NN) :- completar("Ejercicio 9").
+
+% Ejercicio 10
+solucionUnica(NN) :- completar("Ejercicio 10").
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                              %
+%    Ejemplos de nonogramas    %
+%        NO MODIFICAR          %
+%    pero se pueden agregar    %
+%                              %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Fáciles
+nn(0, NN) :- armarNono([[1],[2]],[[],[2],[1]], NN).
+nn(1, NN) :- armarNono([[4],[2,1],[2,1],[1,1],[1]],[[4],[3],[1],[2],[3]], NN).
+nn(2, NN) :- armarNono([[4],[3,1],[1,1],[1],[1,1]],[[4],[2],[2],[1],[3,1]], NN).
+nn(3, NN) :- armarNono([[2,1],[4],[3,1],[3],[3,3],[2,1],[2,1],[4],[4,4],[4,2]], [[1,2,1],[1,1,2,2],[2,3],[1,3,3],[1,1,1,1],[2,1,1],[1,1,2],[2,1,1,2],[1,1,1],[1]], NN).
+nn(4, NN) :- armarNono([[1, 1], [5], [5], [3], [1]], [[2], [4], [4], [4], [2]], NN).
+nn(5, NN) :- armarNono([[], [1, 1], [], [1, 1], [3]], [[1], [1, 1], [1], [1, 1], [1]], NN).
+nn(6, NN) :- armarNono([[5], [1], [1], [1], [5]], [[1, 1], [2, 2], [1, 1, 1], [1, 1], [1, 1]], NN).
+nn(7, NN) :- armarNono([[1, 1], [4], [1, 3, 1], [5, 1], [3, 2], [4, 2], [5, 1], [6, 1], [2, 3, 2], [2, 6]], [[2, 1], [1, 2, 3], [9], [7, 1], [4, 5], [5], [4], [2, 1], [1, 2, 2], [4]], NN).
+nn(8, NN) :- armarNono([[5], [1, 1], [1, 1, 1], [5], [7], [8, 1], [1, 8], [1, 7], [2, 5], [7]], [[4], [2, 2, 2], [1, 4, 1], [1, 5, 1], [1, 8], [1, 7], [1, 7], [2, 6], [3], [3]], NN).
+nn(9, NN) :- armarNono([[4], [1, 3], [2, 2], [1, 1, 1], [3]], [[3], [1, 1, 1], [2, 2], [3, 1], [4]], NN).
+nn(10, NN) :- armarNono([[1], [1], [1], [1, 1], [1, 1]], [[1, 1], [1, 1], [1], [1], [ 1]], NN).
+nn(11, NN) :- armarNono([[1, 1, 1, 1], [3, 3], [1, 1], [1, 1, 1, 1], [8], [6], [10], [6], [2, 4, 2], [1, 1]], [[2, 1, 2], [4, 1, 1], [2, 4], [6], [5], [5], [6], [2, 4], [4, 1, 1], [2, 1, 2]], NN).
+nn(12, NN) :- armarNono([[9], [1, 1, 1, 1], [10], [2, 1, 1], [1, 1, 1, 1], [1, 10], [1, 1, 1], [1, 1, 1], [1, 1, 1, 1, 1], [1, 9], [1, 2, 1, 1, 2], [2, 1, 1, 1, 1], [2, 1, 3, 1], [3, 1], [10]], [[], [9], [2, 2], [3, 1, 2], [1, 2, 1, 2], [3, 11], [1, 1, 1, 2, 1], [1, 1, 1, 1, 1, 1], [3, 1, 3, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 3, 1, 1], [3, 1, 1, 1, 1], [1, 1, 2, 1], [11], []], NN).
+nn(13, NN) :- armarNono([[2], [1,1], [1,1], [1,1], [1], [], [2], [1,1], [1,1], [1,1], [1]], [[1], [1,3], [3,1,1], [1,1,3], [3]], NN).
+nn(14, NN) :- armarNono([[1,1], [1,1], [1,1], [2]], [[2], [1,1], [1,1], [1,1]], NN).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                              %
+%    Predicados auxiliares     %
+%        NO MODIFICAR          %
+%                              %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%! completar(+S)
+%
+% Indica que se debe completar el predicado. Siempre falla.
+completar(S) :- write("COMPLETAR: "), write(S), nl, fail.
+
+%! mostrarNono(+NN)
+%
+% Muestra una estructura nono(...) en pantalla
+% Las celdas x (pintadas) se muestran como ██.
+% Las o (no pintasdas) se muestran como ░░.
+% Las no instanciadas se muestran como ¿?.
+mostrarNono(nono(M,_)) :- mostrarMatriz(M).
+
+%! mostrarMatriz(+M)
+%
+% Muestra una matriz. Solo funciona si las celdas
+% son valores x, o, o no instanciados.
+mostrarMatriz(M) :-
+	M = [F|_], length(F, Cols),
+	mostrarBorde('╔',Cols,'╗'),
+	maplist(mostrarFila, M),
+	mostrarBorde('╚',Cols,'╝').
+
+mostrarBorde(I,N,F) :-
+	write(I),
+	stringRepeat('══', N, S),
+	write(S),
+	write(F),
+	nl.
+
+stringRepeat(_, 0, '').
+stringRepeat(Str, N, R) :- N > 0, Nm1 is N - 1, stringRepeat(Str, Nm1, Rm1), string_concat(Str, Rm1, R).
+
+%! mostrarFila(+M)
+%
+% Muestra una lista (fila o columna). Solo funciona si las celdas
+% son valores x, o, o no instanciados.
+mostrarFila(Fila) :-
+	write('║'),
+	maplist(mostrarCelda, Fila),
+	write('║'),
+	nl.
+
+mostrarCelda(C) :- nonvar(C), C = x, write('██').
+mostrarCelda(C) :- nonvar(C), C = o, write('░░').
+mostrarCelda(C) :- var(C), write('¿?').
